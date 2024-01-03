c0165192902c5f3da6d8c4cd22c712661eb0d69f
zachgalbs <zacharythomasgalbraith@gmail.com>
2024-01-02 18:48:08 -0800

    added christian and popup
    Popup still needs work, but added christian as a person in the 'Our Team' section

--- src/custom.js
+++ src/custom.js
@@ -6,10 +6,28 @@ document.getElementById("tellMeMoreButton").addEventListener('click', function()
 
 let miloButton = document.getElementById("miloButton");
 let oliverButton = document.getElementById("oliverButton");
+let miloModal = document.getElementById("miloModal");
+let oliverModal = document.getElementById("oliverModal");
 
 miloButton.onclick = function() {
-    
+    if (miloModal.style.display == "none" || miloModal.style.display == "") {
+        miloModal.style.display = "block"
+        document.body.classList.add("no-scroll");
+    } 
+    else {
+        miloModal.style.display = "none";
+        document.body.classList.remove("no-scroll");
+    }
 }
 oliverButton.onclick = function() {
     
+}
+window.onclick = function(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+function toggleModal() {
+
 }
