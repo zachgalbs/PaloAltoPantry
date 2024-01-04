923cc7a9c24d012216b2cde1130812349f83fba1
zachgalbs <zacharythomasgalbraith@gmail.com>
2024-01-03 18:38:31 -0800

    brah

--- src/custom.js
+++ src/custom.js
@@ -1,5 +1,5 @@
 document.getElementById("tellMeMoreButton").addEventListener('click', function() {
-    document.getElementById("tellMeMoreTarget").scrollIntoView( {
+    document.getElementById("tellMeMoreTarget").scrollIntoView({
         behavior: "smooth"
     });
 });
@@ -8,26 +8,32 @@ let miloButton = document.getElementById("miloButton");
 let oliverButton = document.getElementById("oliverButton");
 let miloModal = document.getElementById("miloModal");
 let oliverModal = document.getElementById("oliverModal");
+let mainContent = document.getElementById("mainContent");
 
 miloButton.onclick = function() {
-    if (miloModal.style.display == "none" || miloModal.style.display == "") {
-        miloModal.style.display = "block"
-        document.body.classList.add("no-scroll");
-    } 
-    else {
-        miloModal.style.display = "none";
-        document.body.classList.remove("no-scroll");
-    }
+    toggleModal(miloModal);
 }
+
 oliverButton.onclick = function() {
-    
+    toggleModal(oliverModal);
 }
+
 window.onclick = function(event) {
-    if (event.target === modal) {
-        toggleModal();
+    if (event.target === miloModal || event.target === oliverModal) {
+        toggleModal(event.target);
     }
 }
 
-function toggleModal() {
-
+function toggleModal(modal) {
+    if (modal.style.display === "none" || modal.style.display === "") {
+        modal.style.display = "block";
+        document.body.classList.add("no-scroll");
+        mainContent.classList.add('dimmed');
+        modal.classList.add("animateModal");
+    } else {
+        modal.style.display = "none";        
+        document.body.classList.remove("no-scroll");
+        mainContent.classList.remove('dimmed');
+        modal.classList.remove('animate-modal');
+    }
 }
